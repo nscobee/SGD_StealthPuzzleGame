41da9a61a6b4ee60ba73255903e8f44cf6520163
Nicolas R <43824118+onecalledNico@users.noreply.github.com>
2019-03-13 18:04:37 -0500

    Added my basic puzzle elements...
    Backing up my basic puzzle elements before continuing.

---
+++ SGD_Stealth_B2EN2/Assets/Scripts/NickR Stuff/PiecePicker.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PiecePicker : MonoBehaviour
+{
+
+    public bool isPicker;
+    public bool isNotPiece;
+    public bool isThePiece;
+
+    void Start()
+    {
+
+    }
+
+
+    void Update()
+    {
+
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (isPicker == true)
+        {
+            if (other.gameObject.GetComponent<PiecePicker>().isThePiece == true)
+            {
+                Debug.Log("Good Job");
+            }
+            else if (other.gameObject.GetComponent<PiecePicker>().isNotPiece == true)
+            {
+                Debug.Log("Pathetic");
+            }
+        }
+
+
+    }
+
+}
