52bc28ab304cad58ba3b7f0312fcda7ded1e4871
Nicolas R <43824118+onecalledNico@users.noreply.github.com>
2019-03-14 13:00:43 -0500

    Room work...
    Did some more work on my room.

---
+++ SGD_Stealth_B2EN2/Assets/Scripts/NickR Stuff/PictureReader.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PictureReader : MonoBehaviour
+{
+    public string words;
+    public GameObject descPanel;
+    public Text descTxt;
+    bool reading = false;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if (reading == true)
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                Time.timeScale = 1;
+                descTxt.text = "";
+                descPanel.SetActive(false);
+            }
+        }
+    }
+
+    public void OnTriggerStay(Collider other)
+    {
+
+        if (other.CompareTag("Player"))
+        {
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                descTxt.text = words;
+                descPanel.SetActive(true);
+                reading = true;
+                Time.timeScale = 0;
+            }
+
+        }
+
+
+    }
+
+    //public void OnTriggerExit(Collider other)
+    //{
+
+    //    if (other.CompareTag("Player"))
+    //    {
+    //        descTxt.text = "";
+    //        descPanel.SetActive(false);
+    //    }
+
+
+    //}
+}
