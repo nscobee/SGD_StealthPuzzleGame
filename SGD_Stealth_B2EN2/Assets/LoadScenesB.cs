18f279533fd6835c12158804d88c417379b4a1f2
Brandon Foster <soulrise452@gmail.com>
2019-03-13 23:23:07 -0500

    polished menues, connected buttons

---
+++ SGD_Stealth_B2EN2/Assets/LoadScenesB.cs
@@ -0,0 +1,20 @@
+﻿// SceneB.
+// SceneB is given the sceneBuildIndex of 0 which will
+// load SceneA from the Build Settings
+
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadScenesB : MonoBehaviour
+{
+    void Start()
+    {
+        Debug.Log("LoadSceneB");
+    }
+
+    public void LoadB(int sceneANumber)
+    {
+        Debug.Log("sceneBuildIndex to load: " + sceneANumber);
+        SceneManager.LoadScene(sceneANumber);
+    }
+}
