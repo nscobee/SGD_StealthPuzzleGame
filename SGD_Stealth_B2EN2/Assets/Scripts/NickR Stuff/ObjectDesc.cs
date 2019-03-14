19396b3725e46dcd3d170471200c1003b102f42f
Nicolas R <43824118+onecalledNico@users.noreply.github.com>
2019-03-13 19:42:18 -0500

    More puzzle work...
    Did more work on my puzzle, gonna finish the rest tomorrow.

---
+++ SGD_Stealth_B2EN2/Assets/Scripts/NickR Stuff/ObjectDesc.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ObjectDesc : MonoBehaviour
+{
+    public string words;
+    public GameObject panel_Interact;
+    public Text infoWindowTxt;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+       
+        if (other.CompareTag("Player"))
+        {
+            infoWindowTxt.text = words;
+            panel_Interact.SetActive(true);
+        }
+
+
+    }
+
+    public void OnTriggerExit(Collider other)
+    {
+
+        if (other.CompareTag("Player"))
+        {
+            infoWindowTxt.text = "";
+            panel_Interact.SetActive(false);
+        }
+
+
+    }
+}
